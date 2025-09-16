afd2a44e986b7905bc5c75b2e217e3270b5a54ab
JaekwonJo <jaekwonim@gmail.com>
2025-09-17 03:58:16 +0900

    Update Studypage.js

--- client/src/pages/Studypage.js
+++ client/src/pages/Studypage.js
@@ -1,6 +1,4 @@
- /**                                                                                      
-   * StudyPage: 학습/채점/결과 화면을 조정하는 컨테이너                                    
-   */                                                                                      
+                                                                                     
                                                                                            
   import React, { useState, useEffect } from 'react';                                      
   import { useAuth } from '../contexts/AuthContext';                                       
